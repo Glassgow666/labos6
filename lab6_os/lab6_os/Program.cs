6259bb58221763342bd1f1d8dcefecf6f9f37489
Glassgow666 <45860787+Glassgow666@users.noreply.github.com>
2020-12-15 13:26:04 +0300

    Add files via upload

---
+++ lab6_os/lab6_os/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace lab6_os
+{
+    class Program
+    {
+
+		static void DrawCircle()
+		{
+			for (int i = 0; i < 100; i++)
+			{
+				Console.WriteLine("draw circle");
+			}
+
+			return;
+		}
+
+		static void DrawSquare()
+		{
+			for (int i = 0; i < 100; i++)
+			{
+				Console.WriteLine("draw square");
+			}
+			return;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Inside main()");
+			string typeOfShape="square";
+			Console.WriteLine(typeOfShape);
+
+			int i = 0;
+
+			for (; i < 0xffffff; i++) ;
+			switch (typeOfShape)
+			{
+				case "square":
+					DrawSquare();
+					break;
+				case "circle":
+					DrawCircle();
+					break;
+			}
+
+			
+		}
+	}
+}
